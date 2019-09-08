f8222c4209bacc7e41b3e87c51fb2ebd3b79c1a6
alvesfc <alvesfc@protonmail.com>
2019-09-08 02:47:38 -0400

    Created domain classes

---
+++ src/main/java/com/mobiquityinc/domain/ItemDomain.java
@@ -0,0 +1,63 @@
+package com.mobiquityinc.domain;
+
+import java.util.Objects;
+
+
+/**
+ *
+ * Domain class that represents an item.
+ * @author alvesfc
+ * @version 1.0
+ */
+public class ItemDomain {
+
+    private Integer id;
+    private Double weight;
+    private Double cost;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemDomain that = (ItemDomain) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDomain{" +
+                "id=" + id +
+                ", weight=" + weight +
+                ", cost=" + cost +
+                '}';
+    }
+}
---
+++ src/main/java/com/mobiquityinc/domain/PackageDomain.java
@@ -0,0 +1,39 @@
+package com.mobiquityinc.domain;
+
+import java.util.List;
+
+/**
+ *
+ * Domain class that represents a package.
+ * @author alvesfc
+ * @version 1.0
+ */
+public class PackageDomain {
+
+    private double limit;
+    private List<ItemDomain> items;
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+
+    public List<ItemDomain> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemDomain> items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "PackageDomain{" +
+                "limit=" + limit +
+                ", items=" + items +
+                '}';
+    }
+}
